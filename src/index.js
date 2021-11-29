5a2dfa8eefecff6903c02c04f822b8ca66d5dae7
Atharva Johri <atharva.johri@klarna.com>
2021-11-29 02:44:27 +0100

    TINK-2 wire app with redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { ThemeProvider, CSSReset } from '@chakra-ui/core'
 
-import App from "./components/app";
+import App from './components/app'
+
+import store from './store'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <ThemeProvider>
         <CSSReset />
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </ThemeProvider>
     </Router>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
